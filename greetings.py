4dad29676260c56f25484158e2081a6d05fcb7cf
WeI <shiva1981@ya.ru>
2025-01-07 00:04:29 +0800

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,18 @@
+# 1
+username = 'Валерий'
+print("Имя пользователя:", username)
+#2
+title = 'Grade1, Этап1, Задание 1'
+print("Заголовок проекта:", title)
+#3
+content = 'Работа с переменными'
+print("Описание заметки:", content)
+#4
+status = 'Выполняется'
+print("Статус заметки:", status)
+#5
+created_date = '06-01-2025'
+print("Дата создания заметки:", created_date)
+#6
+issue_date = '15-01-2025'
+print("Дата истечения заметки:", issue_date)
