048b4c7e901950ddec58df89d68cb1428fd76564
VeI <shiva1981@ya.ru>
2025-01-07 05:04:53 +0800

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,12 @@
+username = 'Валерий'
+print("Имя пользователя:", username)
+title = 'Grade1, Этап1, Задание 2'
+print("Заголовок проекта:", title)
+content = 'Работа с переменными'
+print("Описание заметки:", content)
+status = 'Выполняется'
+print("Статус заметки:", status)
+created_date = '06-01-2025'
+print("Дата создания заметки:", created_date [0:5])
+issue_date = '15-01-2025'
+print("Дата истечения заметки:", issue_date [0:5])
