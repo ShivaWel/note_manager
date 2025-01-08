dfedd67260744c9612eb84c06a7cf295597b68d4
VeI <shiva1981@ya.ru>
2025-01-09 04:02:13 +0800

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,17 @@
+username = input('Введите имя пользователя:')
+print(username)
+
+title = input('Введите заголовок проекта:')
+print(title)
+
+content = input('Введите описание заметки:')
+print(content)
+
+status = input('Введите статус заметки:')
+print(status)
+
+created_date = input('Дата создания заметки (в формате: число-месяц-год):')
+print(created_date[0:5])
+
+issue_date = input('Дата истечения заметки (в формате: число-месяц-год):')
+print(issue_date[0:5])
